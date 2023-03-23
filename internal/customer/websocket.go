29735a8e82b4aa54767ac17f83a0ccb00bb5e637
buexplain <buexplain@163.com>
2023-03-23 19:24:57 +0800

    更新 Copyright、readme

--- internal/customer/websocket.go
+++ internal/customer/websocket.go
@@ -1,5 +1,5 @@
 /**
-* Copyright 2022 buexplain@qq.com
+* Copyright 2023 buexplain@qq.com
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
