eb7642bf015f00ceca0fe177807d5b50332a5e04
buexplain <buexplain@163.com>
2023-08-18 15:33:55 +0800

    优化 压测模块的连接池

--- test/pkg/utils/connPool/connPool.go
+++ test/pkg/utils/connPool/connPool.go
@@ -6,43 +6,94 @@ import (
 )
 
 type ConnPool struct {
-	pool      chan net.Conn
+	pool chan net.Conn
+	//心跳函数
 	heartbeat func(conn net.Conn) bool
-	factory   func() net.Conn
+	//net.Conn构造函数
+	factory func() net.Conn
+	//用于连接归还失败时候异步构建新连接
+	compensatorCh chan struct{}
 }
 
 func NewConnPool(size int, factory func() net.Conn, heartbeat func(conn net.Conn) bool, heartbeatDuration time.Duration) *ConnPool {
 	cp := &ConnPool{
-		pool:      make(chan net.Conn, size),
-		heartbeat: heartbeat,
-		factory:   factory,
+		pool:          make(chan net.Conn, size),
+		heartbeat:     heartbeat,
+		factory:       factory,
+		compensatorCh: make(chan struct{}, size),
 	}
 	for i := 0; i < size; i++ {
-		cp.pool <- factory()
+		c := factory()
+		if c != nil {
+			cp.pool <- c
+		}
 	}
-	cp.loopHeartbeat(heartbeatDuration)
+	go cp.loopHeartbeat(heartbeatDuration)
+	go cp.compensator()
 	return cp
 }
 
-func (r *ConnPool) loopHeartbeat(heartbeatDuration time.Duration) {
-	go func() {
-		t := time.NewTicker(heartbeatDuration)
-		defer t.Stop()
+// 异步构建一个新连接，给到连接池
+func (r *ConnPool) compensator() {
+	f := func() {
 		for {
-			<-t.C
-			for i := 0; i < cap(r.pool); i++ {
-				conn := <-r.pool
-				if !r.heartbeat(conn) {
-					conn = r.factory()
-				}
-				r.pool <- conn
+			conn := r.factory()
+			if conn == nil {
+				//构建连接失败，间隔1秒再次构建
+				time.Sleep(time.Second * 1)
+				continue
+			}
+			select {
+			case r.pool <- conn:
+				//构建成功，归还到连接池放着
+				return
+			default:
+				//莫名其妙的逻辑导致连接池满了，这里直接丢弃即可
+				_ = conn.Close()
 			}
 		}
-	}()
+	}
+	//监听补偿连接池的连接的信号
+	for {
+		<-r.compensatorCh
+		//收到一个信号，开始补偿连接到连接池
+		f()
+	}
 }
 
-func (r *ConnPool) Factory() net.Conn {
-	return r.factory()
+func (r *ConnPool) loopHeartbeat(heartbeatDuration time.Duration) {
+	t := time.NewTicker(heartbeatDuration)
+	defer t.Stop()
+	heartbeatOk := make(map[net.Conn]struct{}, cap(r.pool))
+	for {
+		<-t.C
+		//遍历pool中的所有连接，并进行心跳处理
+		for i := 0; i < cap(r.pool); i++ {
+			select {
+			case conn := <-r.pool:
+				//拿到连接，判断是否已经发送过心跳
+				if _, ok := heartbeatOk[conn]; ok {
+					break
+				}
+				//没有心跳过，进行一次心跳操作
+				if !r.heartbeat(conn) {
+					//操作失败
+					r.Put(nil)
+				} else {
+					//操作成功，记录起来
+					heartbeatOk[conn] = struct{}{}
+					r.pool <- conn
+				}
+			default:
+				//没拿到连接，说明连接池内的连接被频繁使用，跳过，进行下一次获取连接
+				break
+			}
+		}
+		//清空心跳记录
+		for conn := range heartbeatOk {
+			delete(heartbeatOk, conn)
+		}
+	}
 }
 
 func (r *ConnPool) Get() net.Conn {
@@ -50,5 +101,15 @@ func (r *ConnPool) Get() net.Conn {
 }
 
 func (r *ConnPool) Put(conn net.Conn) {
-	r.pool <- conn
+	if conn == nil {
+		//连接归还失败，为了避免连接池在此情况下被掏空，进而导致Get连接操作被阻塞，这里发送一个异步构建新连接的信号
+		select {
+		case r.compensatorCh <- struct{}{}:
+			return
+		default:
+			return
+		}
+	} else {
+		r.pool <- conn
+	}
 }
--- test/pkg/utils/pool.go
+++ test/pkg/utils/pool.go
@@ -15,8 +15,10 @@ import (
 var pool *connPool.ConnPool
 var logger zerolog.Logger
 
+// InitPool 初始化与网关交互所需的连接池
 func InitPool(size int, workerListenAddress string) {
 	pool = connPool.NewConnPool(size, func() net.Conn {
+		//连接到网关的worker服务器
 		conn, err := net.Dial("tcp", workerListenAddress)
 		if err != nil {
 			logger.Error().Err(err).Type("errorType", err).Msg("Business client connect worker service failed")
@@ -24,6 +26,7 @@ func InitPool(size int, workerListenAddress string) {
 		}
 		return conn
 	}, func(conn net.Conn) bool {
+		//发送心跳字符串
 		bf := make([]byte, 4+len(netsvrProtocol.PingMessage))
 		binary.BigEndian.PutUint32(bf, uint32(len(netsvrProtocol.PingMessage)))
 		copy(bf[4:], netsvrProtocol.PingMessage)
@@ -32,6 +35,7 @@ func InitPool(size int, workerListenAddress string) {
 			logger.Error().Err(err).Type("errorType", err).Msg("Business send to worker failed")
 			return false
 		}
+		//接收心跳字符
 		data := make([]byte, 4+len(netsvrProtocol.PongMessage))
 		if _, err = io.ReadFull(conn, data); err != nil {
 			logger.Error().Err(err).Type("errorType", err).Msg("Business read from worker failed")
@@ -41,14 +45,17 @@ func InitPool(size int, workerListenAddress string) {
 	}, time.Second*45)
 }
 
+// RequestNetSvr 负责与网关进程交互的函数，类似于http请求方法，发送一个req请求，接收一个resp响应
 func RequestNetSvr(req proto.Message, cmd netsvrProtocol.Cmd, resp proto.Message) {
+	//构建一个发送到网关的router对象
 	router := &netsvrProtocol.Router{}
 	router.Cmd = cmd
 	if req != nil {
+		//请求体不为空，则将其编码到router对象上
 		router.Data, _ = proto.Marshal(req)
 	}
 	pt, _ := proto.Marshal(router)
-	//写数据
+	//把router对象的protobuf数据，按大端序构造一个buf
 	bf := &bytes.Buffer{}
 	length := len(pt)
 	bf.WriteByte(byte(length >> 24))
@@ -60,29 +67,53 @@ func RequestNetSvr(req proto.Message, cmd netsvrProtocol.Cmd, resp proto.Message
 		logger.Error().Err(err).Type("errorType", err).Msg("Business send to worker buffer failed")
 		return
 	}
+	//获取与网关建立好的连接
 	conn := pool.Get()
-	defer pool.Put(conn)
-	_, err = bf.WriteTo(conn)
-	if err != nil {
-		logger.Error().Err(err).Type("errorType", err).Msg("Business send to worker failed")
+	//将buf数据发送到网关
+	for {
+		_, err = bf.WriteTo(conn)
+		if err != nil {
+			//写入失败，tcp短写，则继续写入
+			if err == io.ErrShortWrite {
+				continue
+			}
+			//写入数据失败，视为连接已经被损坏，归还一个空连接到连接池
+			pool.Put(nil)
+			logger.Error().Err(err).Type("errorType", err).Msg("Business send to worker failed")
+			return
+		}
+		break
+	}
+	//不需要接收数据，直接退出
+	if resp == nil {
+		//归还连接
+		pool.Put(conn)
 		return
 	}
-	//读数据
+	//需要接收网关返回的数据数据
 loop:
 	dataLenBuf := make([]byte, 4)
 	if _, err = io.ReadFull(conn, dataLenBuf); err != nil {
+		//读取失败，归还一个空连接到连接池
+		pool.Put(nil)
 		logger.Error().Err(err).Type("errorType", err).Msg("Business read from worker failed")
 		return
 	}
 	dataLen := binary.BigEndian.Uint32(dataLenBuf)
 	dataBuf := make([]byte, dataLen)
 	if _, err = io.ReadAtLeast(conn, dataBuf, int(dataLen)); err != nil {
+		//读取失败，归还一个空连接到连接池
+		pool.Put(nil)
 		logger.Error().Err(err).Type("errorType", err).Msg("Business read from worker failed")
 		return
 	}
+	//跳过心跳字符，继续读取数据
 	if bytes.Equal(netsvrProtocol.PongMessage, dataBuf) {
 		goto loop
 	}
+	//归还连接
+	pool.Put(conn)
+	//解析网关返回的数据
 	router.Reset()
 	if err = proto.Unmarshal(dataBuf, router); err != nil {
 		logger.Error().Err(err).Type("errorType", err).Msg("Proto unmarshal internalProtocol.Router failed")
